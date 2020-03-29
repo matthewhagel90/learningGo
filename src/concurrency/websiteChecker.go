7ecffb92be1ed3e41cb89705e252097d08c9724e
matthew.hagel <matthew.hagel@imaginecurve.com>
2020-03-29 16:37:04 +0100

    concurrency

---
+++ src/concurrency/websiteChecker.go
@@ -0,0 +1,28 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultsChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultsChannel <- result{
+				string: u,
+				bool:   wc(u),
+			}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultsChannel
+		results[result.string] = result.bool
+	}
+
+	return results
+}
